0907106e8380ab550a9a1f808507f8cb14f5120d
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2015-01-12 19:49:32 +0100

    Setting up BackgroundImage

---
+++ PresentationScreen/Assets/Resources/Scripts/GUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/**
+ * Class that handles the GUI of the BlinkenTiles Presentation Screen.
+ * 
+ * Class Interaction Design 2014/2015, Hochschule Furtwangen University
+ * Author: Sarah Haefele
+ **/
+
+public class GUI : MonoBehaviour {
+	//creating custom GUI style
+	GUIStyle myGUIStyle = new GUIStyle();
+	private Graphic backgroundImage;
+
+
+	void Start () {
+		//enabeling richtext
+		myGUIStyle.richText = true;
+
+		//set backgroundImage by loading the prefab
+		backgroundImage = (Graphic)Resources.Load("Prefabs/BackgroundImage", typeof(Graphic));
+		if (backgroundImage == null) {
+			Debug.Log ("Couldn't find backgroundImage");
+		} else {
+			Debug.Log("Found Image.");
+		}
+		//Instantiate background Image
+		//Instantiate (backgroundImage, new Vector3(x,y,0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
