3a4fd9462cd2e63702c050c7c9ea79c6041a9f37
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2015-01-18 14:34:26 +0100

    Added Document class

---
+++ PresentationScreen/Assets/Resources/Scripts/HowTo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HowTo : MonoBehaviour {
+
+	/*Boolean that determines if scene is ready to be exited*/
+	public bool sceneFinished = false;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("HowTo-Scene loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
