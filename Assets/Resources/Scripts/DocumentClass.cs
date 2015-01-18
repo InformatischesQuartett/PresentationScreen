9e96e50385f47f920711552f17d31df80ba0d78e
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2015-01-18 16:09:36 +0100

    Build GUI - not finished

--- Assets/Resources/Scripts/DocumentClass.cs
+++ Assets/Resources/Scripts/DocumentClass.cs
@@ -7,9 +7,9 @@ using System.Collections;
  **/ 
 public class DocumentClass : MonoBehaviour {
 	private Texture2D _backgroundTex;
+	private Texture2D _guiUnderlayTex;
 
-	public int toolbarInt = 0;
-	public string[] toolbarStrings = new string[] {"StartMenu", "HowTo", "Toolbar3"};
+	private bool _enableGUI = true;
 	
 	void Awake () {
 		DontDestroyOnLoad(this);
@@ -17,33 +17,52 @@ public class DocumentClass : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_backgroundTex = (Texture2D) Resources.Load ("Textures/background", typeof(Texture2D));
-
-		Debug.Log (_backgroundTex);
+		_guiUnderlayTex = (Texture2D) Resources.Load ("Textures/guiUnderlay", typeof(Texture2D));
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		/*Turning GUI on and off*/
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			_enableGUI = !_enableGUI;
+		}
+
+		/*Switching Levels*/
+		if (Input.GetKeyDown (KeyCode.Keypad1) || Input.GetKeyDown (KeyCode.Alpha1)) {
+			Debug.Log("Loading Level: HowTo");
+			Application.LoadLevel("MainMenu");
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad2) || Input.GetKeyDown (KeyCode.Alpha2)) {
+			Debug.Log("Loading Level: HowTo");
+			Application.LoadLevel("HowTo");
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad3) || Input.GetKeyDown (KeyCode.Alpha2)) {
+			Debug.Log("Loading Level: Behind the Scenes");
+			//Application.LoadLevel("BehindTheScenes");
+		}
+
 	
 	}
 
 	void OnGUI () {
-		//Drav Background Texture
+		//Draw Background Texture
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), _backgroundTex);
 
-		toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings);
+		if (_enableGUI) {
 
-		switch (toolbarInt) {
-			case 0:
-				Application.LoadLevel("MainMenu");
-				break;
-			case 1:
-				Application.LoadLevel("HowTo");
-				break;
-			case 2:
-				break;
-		}//end switch
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height/5), _guiUnderlayTex);
+			GUI.Label (new Rect (10,10, 80, 50), "Main Menu: 1");
+			GUI.Label (new Rect (100,10, 80, 50), "How To: 2");
+			GUI.Label (new Rect (190,10, 80, 50), "Behind the Scenes: 3");
+		}
 
 
+
+				
+				//Application.LoadLevel("MainMenu");
+				
+				//Application.LoadLevel("HowTo");
 
 	}//end OnGUI
 }
