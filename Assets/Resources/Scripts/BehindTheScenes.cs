6505f103dd1a565a792fb153bedc5d3706dcb2af
Fabian Gaertner <fabian.gaertner@hs-furtwangen.de>
2015-01-19 00:20:07 +0100

    Merge + added MovieTexture

--- Assets/Resources/Scripts/BehindTheScenes.cs
+++ Assets/Resources/Scripts/BehindTheScenes.cs
@@ -10,14 +10,19 @@ public class BehindTheScenes : MonoBehaviour {
 	private float _updateTimer;
 
 	private DocumentClass docClass;
+	
+    	private MovieTexture _movieTexture;
 
-	private const string path = @"C:\Users\Fabian\Dropbox\HS Furtwangen\Master\1. Semester\Interaktionsdesign\BlinkenTiles\Assets\StreamingAssets\Network";
+    private const string path = @"E:\Dropbox\HS Furtwangen\Master\1. Semester\Interaktionsdesign\BlinkenTiles\Assets\StreamingAssets\Network";
 	
 	private void Start () {
 		_imageFiles = new Texture2D[4];
 
 		_updateStep = 0;
 		_updateTimer = 0;
+
+        _movieTexture = Resources.Load<MovieTexture>("Textures/BehindTheScenes");
+        _movieTexture.Play();
 
 		LoadImages();
 
@@ -37,34 +42,33 @@ public class BehindTheScenes : MonoBehaviour {
 	}
 
 	private void OnGUI() {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), _movieTexture);
+
 		var sizeFactor = Screen.width/1920f;
 
-		if (_updateTimer > 0)
-			GUI.DrawTexture(new Rect(288 * sizeFactor, 100 * sizeFactor, 384 * sizeFactor, 318 * sizeFactor), _imageFiles[0]);
+	    var imgWidth = 413*sizeFactor;
+        var imgHeight = imgWidth * 0.8f;
+
+	    var imgLeft = 510*sizeFactor;
+	    var imgTop = 288*sizeFactor;
+	    var imgRight = 993*sizeFactor;
+	    var imgBottom = 681*sizeFactor;
 
 		if (_updateTimer > 4)
-			GUI.DrawTexture(new Rect(1248 * sizeFactor, 100 * sizeFactor, 384 * sizeFactor, 318 * sizeFactor), _imageFiles[1]);
-
-		if (_updateTimer > 8)
-			GUI.DrawTexture(new Rect(1248 * sizeFactor, 600 * sizeFactor, 384 * sizeFactor, 318 * sizeFactor), _imageFiles[2]);
+            GUI.DrawTexture(new Rect(imgLeft, imgTop, imgWidth, imgHeight), _imageFiles[0]);
 
 		if (_updateTimer > 12)
-			GUI.DrawTexture(new Rect(288 * sizeFactor, 600 * sizeFactor, 384 * sizeFactor, 318 * sizeFactor), _imageFiles[3]);
+			GUI.DrawTexture(new Rect(imgRight, imgTop, imgWidth, imgHeight), _imageFiles[1]);
 
-		if (_updateTimer > 17)
+		if (_updateTimer > 20)
+			GUI.DrawTexture(new Rect(imgRight, imgBottom, imgWidth, imgHeight), _imageFiles[2]);
+
+		if (_updateTimer > 28)
+			GUI.DrawTexture(new Rect(imgLeft, imgBottom, imgWidth, imgHeight), _imageFiles[3]);
+
+		if (_updateTimer > 32)
 		{
-			_updateTimer = 0;
-			_updateStep++;
-
-			if (_updateStep == 3)
-			{
-				/*Go to next Scene*/
-				docClass.LoadNextScene();
-
-				return;
-			}
-
-			LoadImages();
+			docClass.LoadNextScene();
 		}
 
 		_updateTimer += Time.deltaTime;
